a6969c0ef60b946199a5267a8e0981de7d4c4aa1
urodstvo <dead.kanji@mail.ru>
2024-12-03 16:26:37 +0300

    fix: deleted question folder

--- apps/survey/src/grpc_impl/question/__init__.py
+++
@@ -1,5 +0,0 @@
-from .add_to_survey import AddTagsToSurvey
-from .delete_from_survey import DeleteTagsFromSurvey
-from .get_for_survey import GetSurveyTags
-
-__all__ = ["AddTagsToSurvey", "DeleteTagsFromSurvey", "GetSurveyTags"]
